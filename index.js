8e4fa52baf29f802bfef2dec065c87ca2688234e
geverettQo <geverett@quikorder.com>
2019-06-25 22:00:56 -0500

    starting a seeder

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const bodyParser = require('body-parser');
 const express = require('express');
 //const {Collection} = require('./db/index');
 
-var app = express();
-app.use(bodyParser.json());
-app.use(require('./router'));
 
-app.listen(3000, () => {
-  console.log('listening on 3000')
-});
+module.exports = new Promise(async (resolve) => {
+
+  var app = express();
+  app.use(bodyParser.json());
+  app.use(require('./router'));
+
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+    resolve(app)
+  });
+
+})
